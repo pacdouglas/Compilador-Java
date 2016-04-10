3865eae87a32df4a97e075c453b69bb89fd8ec4d
Douglas Martins <douglas.pac@gmail.com>
2016-04-10 19:32:24 -0300

    FileLoader Implementado

--- src/br/com/compilador/main/FileLoader.java
+++ src/br/com/compilador/main/FileLoader.java
@@ -1,4 +1,9 @@
 package br.com.compilador.main;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /*
  *  classe usada pelo AnLexico, 
  *  responsável pela leitura do arquivo de entrada. 
@@ -7,5 +12,66 @@ package br.com.compilador.main;
  */
 
 public class FileLoader {
+	public BufferedReader buffer;
+
+	private int linha;
+	private int coluna;
+	private int ultimaColuna;
+
+	private boolean finalLinha = false;
+
+	// Construtor
+	public FileLoader(String path) throws IOException {
+		this.linha = 1;
+		this.coluna = 0;
+		this.buffer = new BufferedReader(new FileReader(path));
+	}
 	
+	// Retorna o proximo char
+	public char getNextChar() throws IOException {
+		this.buffer.mark(1);
+		char aux = (char) this.buffer.read();
+		this.controlLineColumn(aux);
+		return aux;
+	}
+
+	// Controla contadores das linhas e colunas
+	public void controlLineColumn(char c) {
+		if (this.finalLinha) {
+			this.linha++;
+			this.coluna = 0;
+			this.finalLinha = false;
+		}
+		
+		this.setUltimaColuna(this.coluna);
+		
+		if (c == '\n') {
+			this.finalLinha = true;
+		}
+		this.coluna++;
+	}
+
+	// Retorna Linha Atual
+	public int getLine() {
+		return linha;
+	}
+
+	// Retorna Coluna Atual
+	public int getColumn() {
+		return coluna;
+	}
+
+	// Retornar o elemento atual em String
+	public String getElement() {
+		return "[" + this.getLine() + "," + this.getColumn() + "]";
+	}
+
+	public int getUltimaColuna() {
+		return ultimaColuna;
+	}
+
+	public void setUltimaColuna(int ultimaColuna) {
+		this.ultimaColuna = ultimaColuna;
+	}
+
 }
--- src/br/com/compilador/main/Main.java
+++ src/br/com/compilador/main/Main.java
@@ -1,8 +1,49 @@
 package br.com.compilador.main;
 
+import java.io.IOException;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		FileLoader teste = new FileLoader("C:\\Users\\dougl\\workspace\\Compilador-Java\\arquivo.txt");
+		
+		
+		char t = teste.getNextChar();
+		System.out.println(t+" "+teste.getColumn()+ " " + teste.getLine());
+		t = teste.getNextChar();
+		System.out.println(t+"  "+teste.getColumn()+ " " + teste.getLine());
+		t = teste.getNextChar();
+		System.out.println(t+"  "+teste.getColumn()+ " " + teste.getLine());
+		t = teste.getNextChar();
+		System.out.println(t+"  "+teste.getColumn()+ " " + teste.getLine());
+		t = teste.getNextChar();
+		System.out.println(t+"  "+teste.getColumn()+ " " + teste.getLine());
+		t = teste.getNextChar();
+		System.out.println(t+"  "+teste.getColumn()+ " " + teste.getLine());
+		t = teste.getNextChar();
+		System.out.println(t+"  "+teste.getColumn()+ " " + teste.getLine());
+		t = teste.getNextChar();
+		System.out.println(t+"  "+teste.getColumn()+ " " + teste.getLine());
+		t = teste.getNextChar();
+		System.out.println(t+"  "+teste.getColumn()+ " " + teste.getLine());
+		t = teste.getNextChar();
+		System.out.println(t+"  "+teste.getColumn()+ " " + teste.getLine());
+		t = teste.getNextChar();
+		System.out.println(t+"  "+teste.getColumn()+ " " + teste.getLine());
+		t = teste.getNextChar();
+		System.out.println(t+"  "+teste.getColumn()+ " " + teste.getLine());
+		t = teste.getNextChar();
+		System.out.println(t+"  "+teste.getColumn()+ " " + teste.getLine());
+		t = teste.getNextChar();
+		System.out.println(t+"  "+teste.getColumn()+ " " + teste.getLine());
+		t = teste.getNextChar();
+		System.out.println(t+"  "+teste.getColumn()+ " " + teste.getLine());
+		t = teste.getNextChar();
+		System.out.println(t+"  "+teste.getColumn()+ " " + teste.getLine());
+		t = teste.getNextChar();
+		System.out.println(t+"  "+teste.getColumn()+ " " + teste.getLine());
+		t = teste.getNextChar();
+		
 		
 	}
 
